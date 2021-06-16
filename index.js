622127530d10d8bea8f60ca9673b35859c14197e
Quinn Lonergan <qui99lo@gmail.com>
2021-06-16 15:23:56 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(Number1,Number2) {
+    return Number1 + Number2;
+}
+function subtract(Number1,Number2) {
+    return Number1 - Number2;
+}
+function multiply(Number1,Number2) {
+    return Number1 * Number2;
+}
+function divide(Number1,Number2) {
+    return Number1 / Number2;
+}
+function increment(n) {
+    return n += 1;
+}
+function decrement(n) {
+    return n -= 1;
+}
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
